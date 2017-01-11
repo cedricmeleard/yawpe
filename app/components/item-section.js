6e19badd47910f600ff8c11d7ff96bdc104bb234
Cédric Méléard <cedric.meleard@gmail.com>
2017-01-11 23:55:50 +0100

    add markdown

--- app/components/item-section.js
+++ app/components/item-section.js
@@ -2,13 +2,13 @@
  * Created by cedric on 08/01/17.
  */
 Vue.component('item-section', {
-    props : ['item', 'part'],
-    data : function() {
+    props: ['item', 'part'],
+    data: function () {
         return {
-            sections : this.item.sections
+            sections: this.item.sections
         };
     },
-    computed : {
+    computed: {
         section () {
             if (this.item.sections) {
                 return this.item.sections[this.part];
@@ -16,12 +16,21 @@ Vue.component('item-section', {
             else {
                 return this.item;
             }
+        },
+        contentText(){
+            if (Array.isArray(this.section.content)) {
+                var lines = this.section.content.map(line => markdown.toHTML(line));
+                return lines.join(" ");
+            }
+            else
+                return markdown.toHTML(this.section.content);
         }
     },
     template: `
         <section class="item-section">        
             <header v-if="section.title" class="section-header">{{ section.title }}</header>
-            <article v-if="section.content" class="section-content">{{ section.content }}</article>
+            <article v-if="section.content" class="section-content" v-html="contentText"></article>
             <footer class="section-footer"></footer>        
         </section>`
-});
+})
+
